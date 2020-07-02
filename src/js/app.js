b7c5e69897c99e81a7cdc612b2d1e34245208861
kiselevgleb <kiselevgleb@gmail.com>
2020-07-02 23:33:29 +0300

    added index and fixed error

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,13 @@
-const div = document.getElementById("div");
-const button = document.querySelector("button");
+const div = document.getElementById('div');
+const button = document.querySelector('button');
 
-button.addEventListener('click', (event) => {
-  if (div.children[0].style.display != "block") {
-    div.children[0].style.display = "block";
+button.addEventListener('click', () => {
+  if (div.children[0].style.display !== 'block') {
+    div.children[0].style.display = 'block';
     div.children[0].children[0].innerText = button.dataset.title;
     div.children[0].children[1].innerText = button.dataset.content;
-    console.log(div.children[0].offsetWidth);
-    console.log(button.offsetWidth);
-
-    div.children[0].style.left =  `${button.offsetLeft-((div.children[0].offsetWidth-button.offsetWidth)/2)}px`;
-    console.log(button.offsetLeft);
-    console.log(div.children[0].style.left);
-
+    div.children[0].style.left = `${button.offsetLeft - ((div.children[0].offsetWidth - button.offsetWidth) / 2)}px`;
   } else {
-    div.children[0].style.display = "none";
+    div.children[0].style.display = 'none';
   }
 });
